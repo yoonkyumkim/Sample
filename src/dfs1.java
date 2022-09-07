a88dd69c8bb6693fe0a6fb83b8b0373320015824
Yoon Kyum <kukkkkkk@naver.com>
2022-09-07 15:52:44 +0900

    graph dfs stack 추가

--- src/dfs1.java
+++ src/dfs1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class dfs1 {
 
@@ -7,6 +8,8 @@ public class dfs1 {
     static int[][] Graph = new int[MAX_N][MAX_N];
     static boolean[] Visited = new boolean[MAX_N];
 
+    //input 5 6
+    //0 1 0 2 1 3 1 4 2 4 3 4
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
@@ -26,6 +29,26 @@ public class dfs1 {
         for (int next = 0; next < N; ++next) {
             if (!Visited[next] && Graph[node][next] != 0)
                 dfs(next);
+        }
+    }
+
+    static void stackDfs(int node) {
+        boolean[] visited = new boolean[MAX_N];
+        Stack<Integer> mystack = new Stack<>();
+        mystack.push(node);
+
+        while (!mystack.empty()) {
+            int curr = mystack.pop();
+
+            if (visited[curr]) continue;
+
+            visited[curr] = true;
+            System.out.println(curr + " ");
+
+            for (int next = 0; next < N; ++next) {
+                if (!visited[next] && Graph[curr][next] != 0)
+                    mystack.push(next);
+            }
         }
     }
 
