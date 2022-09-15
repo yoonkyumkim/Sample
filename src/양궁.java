14ad15c4bbc8802ae7005b7de59065e63f8020f2
Yoon Kyum <kukkkkkk@naver.com>
2022-09-15 17:36:31 +0900

    카카오 양궁 풀이

---
+++ src/양궁.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 양궁 {
+    static int result = -1;
+    static int[] answer = new int[11];
+    static int maxDiff;
+
+    public int[] solution(int n, int[] info) {
+        int[] app = new int[11];
+        dfs(0, info, app, n);
+        int sum = 0;
+        for (int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i]);
+            sum += answer[i];
+        }
+        if (sum > 0)
+            return answer;
+        else
+            return new int[]{-1};
+    }
+
+
+    public static void dfs (int idx, int[] lion, int[] app, int arrows) {
+        if (idx > 10 || arrows == 0) {
+            app[10] += arrows;
+            calc(lion, app);
+            app[10] -= arrows;
+            return;
+        }
+
+        if (lion[idx] < arrows) {
+            app[idx] += lion[idx] + 1;
+            dfs (idx + 1, lion, app, arrows - app[idx]);
+            app[idx] -= lion[idx] + 1;
+        }
+        dfs (idx + 1, lion, app ,arrows);
+    }
+    public static void calc(int[] lion, int[] app) {
+        int l = 0;
+        int a = 0;
+        for (int i = 0; i < lion.length; i++) {
+            if (lion[i] > app[i])
+                l += 10 - i;
+            else if (lion[i] + app[i] > 0)
+                a += 10 - i;
+        }
+        int diff = a - l;
+        if (diff > 0 && diff >= maxDiff) {
+            if (maxDiff == diff && !isBetter(app))
+                return;
+
+            maxDiff = diff;
+            answer = Arrays.stream(app).toArray();
+
+        }
+    }
+
+    static boolean isBetter(int[] app) {
+        for(int i=10; i>=0; i--) {
+            if(app[i] > answer[i]) return true;
+            else if(app[i] < answer[i]) return false;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        양궁 y = new 양궁();
+        //int[] info = {2,1,1,1,0,0,0,0,0,0,0};
+        int[] info = {0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3};
+        y.solution(10 , info);
+    }
+}
