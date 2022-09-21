564d630045d72273927f42197a4d9cdaad43961e
Yoon Kyum <kukkkkkk@naver.com>
2022-09-22 02:08:02 +0900

    단순 재귀 아래 링크를 참고하자
    https://lts0606.tistory.com/509

--- src/factorial.java
+++ src/factorial.java
@@ -3,7 +3,6 @@ public class factorial {
     public static void main(String[] args) {
         System.out.println("factorial : "+factorial(3));
     }
-
     public static int factorial(int i) {
         if (i == 1) {
             return 1;
