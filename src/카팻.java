ba74f98a8d132d509f94022c648352a6c6c5ea3b
Yoon Kyum <kukkkkkk@naver.com>
2022-09-21 17:27:40 +0900

    프로그래머스 카팻 문제
    지문이해가 의외로 어려웠던 문제
    
    https://school.programmers.co.kr/learn/courses/30/lessons/42842

---
+++ src/카팻.java
@@ -0,0 +1,32 @@
+public class 카팻 {
+
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+
+
+        int sum = brown + yellow;
+        int y = 0;
+        int center;
+        for (int i = 3; i < sum; i++) {
+            y = sum / i;
+            //약수가 아님
+            if (sum % i > 0 && y < 3) {
+                continue;
+            }
+            center = (i - 2) * (y - 2);
+            if (center == yellow && i >= y) {
+                answer[0] = i;
+                answer[1] = y;
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        카팻 k = new 카팻();
+        k.solution(10, 2);
+    }
+
+}
