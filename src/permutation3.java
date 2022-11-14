301e1aedbc3d92517588c8765f1b2f6bdc102d6f
Yoon Kyum <kukkkkkk@naver.com>
2022-11-14 22:55:57 +0900

    수열3 리딩

--- src/permutation3.java
+++ src/permutation3.java
@@ -26,13 +26,11 @@ public class permutation3 {
    private static void reculsion(List<String> arr, List<String> result, int n, int r) {
 
       if (r == 0) {
-
          System.out.println(result.toString());
          return;
       }
 
       for (int i = 0; i < n; i++) {
-
          result.add(arr.remove(i));
          reculsion(arr, result, n - 1, r - 1);
          arr.add(i, result.remove(result.size() - 1));
