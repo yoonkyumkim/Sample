7af8dd70da9c9b08ad76f40ecaa29f098db17eb0
Yoon Kyum <kukkkkkk@naver.com>
2022-11-21 17:09:22 +0900

    인터페이스

--- src/Exam/인터샘플.java
+++ src/Exam/인터샘플.java
@@ -2,8 +2,8 @@ package Exam;
 
 public interface 인터샘플 {
 
+    //인터페이스는 접근제어자를 안적으면 public으로 적용
     public void sample();
-
     private void sample2() {
         System.out.println("프라이빗");
     }
